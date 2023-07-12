4adbdb456c827ba7f3527e039c28787a208b5fd2
Mustafa Vural <vuralmustafa474@gmail.com>
2023-07-12 16:41:23 +0300

    STM32F103C6 Timer ile led yakıp söndürme
    Timer'i kullanarak osilasyonda ledin yanıp söndüğünü sinyal grafiğini görme ve normal olarak ledin yanıp sönmesini görme. Program olarak keil v5 kullandım. Tasarım kısmını prıteusta yaptım

---
+++ TIM_OC_103C6/MDK-ARM/RTE/_TIM_OC_103C6/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'TIM_OC_103C6' 
+ * Target:  'TIM_OC_103C6' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ TIM_OC_103C6/MDK-ARM/TIM_OC_103C6/startup_stm32f103x6.d
@@ -0,0 +1,1 @@
+tim_oc_103c6\startup_stm32f103x6.o: startup_stm32f103x6.s
